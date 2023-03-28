59fb30c5cfa5dc05247cb831929fbe5b960d33de
Ba Phu <phunguyen13042001@gmail.com>
2023-03-28 22:05:52 +0700

    first commit

---
+++ pages/footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+         <h2> Nguyễn Bá Phú </h2>
+       
+         <h3>Thanh Ba - Phú Thọ</h3>
+         <h4>DHTI13A5HN</h4>
+        
+        </p>
+    </div>
---
+++ pages/header.php
@@ -0,0 +1,3 @@
+<div class="header">
+    <img src="./images/phukien.jpg" alt="" style="height: 180px ;width: 100%;" >
+    </div>
